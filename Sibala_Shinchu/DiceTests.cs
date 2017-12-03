b356b1f28209305c7e1c94c57c9ff8eeff8dd19c
esawei <esawei5190@gmail.com>
2017-12-03 11:14:26 +0800

    新增成功測試案例

--- Sibala_Shinchu/DiceTests.cs
+++ Sibala_Shinchu/DiceTests.cs
@@ -21,6 +21,22 @@ namespace Sibala_Hsinchu
             var expected = 1;
             CompareAreEqual(diceA, diceB, expected);
         }
+        [TestMethod]
+        public void CompareTest_1_Point_1_NoPoint_should_be_1()
+        {
+            var diceA = new DiceDto(1, DiceDto.StatusEnum.Points);
+            var diceB = new DiceDto(1, DiceDto.StatusEnum.NoPoints);
+            var expected = 1;
+            CompareAreEqual(diceA, diceB, expected);
+        }
+        [TestMethod]
+        public void CompareTest_1_Same_1_NoPoint_should_be_1()
+        {
+            var diceA = new DiceDto(1, DiceDto.StatusEnum.SameColor);
+            var diceB = new DiceDto(1, DiceDto.StatusEnum.NoPoints);
+            var expected = 1;
+            CompareAreEqual(diceA, diceB, expected);
+        }
 
         private static void CompareAreEqual(DiceDto diceA, DiceDto diceB, int expected)
         {
