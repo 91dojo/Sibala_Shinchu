a4fad698ea758838c157020d4a2879a2a0677a61
Brian <hatedinny1@gmail.com>
2017-12-03 11:27:09 +0800

    refactor 1. no points 2. 5 points

--- Sibala_Shinchu/SibalaTest.cs
+++ Sibala_Shinchu/SibalaTest.cs
@@ -7,14 +7,30 @@ namespace Sibala_Hsinchu
     [TestClass]
     public class SibalaTest
     {
+        private IEnumerable<int> input;
+
         [TestMethod]
         public void GetResult_input_1_2_3_4_should_be_no_points()
         {
-            var input = new List<int> {1, 2, 3, 4};
+            input = new List<int> {1, 2, 3, 4};
             var target = new Sibala();
             var actual = target.GetResult(input);
             var expected = "no points";
             Assert.AreEqual(expected,actual);
+        }
+        [TestMethod]
+        public void GetResult_input_1_2_3_1_Should_Be_5_points()
+        {
+            //arrange
+            input = new List<int> { 1, 2, 3, 1 };
+            var target = new Sibala();
+            var expected = "5 points";
+
+            //act
+            var actual = target.GetResult(input);
+            
+            //assert
+            Assert.AreEqual(expected, actual);
         }
     }
 }
