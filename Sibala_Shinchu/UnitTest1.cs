6854794c204659ae670d20ad8c8643f8bfecd210
Brian <hatedinny1@gmail.com>
2017-12-03 10:14:29 +0800

    first pass test : implement no points

--- Sibala_Shinchu/UnitTest1.cs
+++ Sibala_Shinchu/UnitTest1.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Collections.Generic;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Sibala_Hsinchu
 {
@@ -8,7 +10,19 @@ namespace Sibala_Hsinchu
         [TestMethod]
         public void TestMethod1()
         {
+            var input = new List<int> {1, 2, 3, 4};
+            var target = new Sibala();
+            var actual = target.GetResult(input);
+            var expected = "no points";
+            Assert.AreEqual(expected,actual);
+        }
+    }
 
+    public class Sibala
+    {
+        public string GetResult(List<int> input)
+        {
+            return "no points";
         }
     }
 }
